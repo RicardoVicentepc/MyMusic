2999355b42b82c782204b02dd6179ed0d17a7ffc
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2023-05-20 20:52:40 -0300

    web data viz

---
+++ src/models/pessoaModel.js
@@ -0,0 +1,35 @@
+var database = require("../database/config");
+
+function listar() {
+    console.log("ACESSEI O Pessoa MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+
+    var instrucao = `SELECT * FROM pessoa;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucao)
+    return database.executar(instrucao);
+}
+
+
+function entrar(email, senha) {
+    console.log("ACESSEI O Pessoa MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha);
+
+    var instrucao =
+        `SELECT * FROM pessoa WHERE email = '${email}' AND senha = '${senha}'`;
+
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(nome, sobreNome, email, senha) {
+    var instrucao =
+        `INSERT INTO pessoa (nome, sobreNome, email, senha) values 
+        ('${nome}', '${sobreNome}', '${email}', '${senha}')`;
+    console.log("Executando a instrução SQL : \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar
+};
---
+++ src/routes/pessoa.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var pessoaController = require("../controllers/pessoaController");
+
+router.get("/", function (req, res){
+    pessoaController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    pessoaController.listar(req, res);
+});
+
+router.post("/cadastrar", function(req, res){
+    pessoaController.cadastrar(req, res);
+});
+
+router.post("/autenticar", function(req, res) {
+    pessoaController.entrar(req, res)
+});
+
+module.exports = router;
